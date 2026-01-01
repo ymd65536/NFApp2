58d5d7ff842c9dd3ef973e4f2c243a050b90b772
ymd65536 <ymd65536@gmail.com>
2026-01-02 07:20:37 +0900

    fix: reduce sleep durations for avatar expression demonstrations

--- Program.cs
+++ Program.cs
@@ -17,22 +17,22 @@ avatar.Start();
 Console.WriteLine("Avatar initialized and started");
 
 // Demonstrate different expressions
-Thread.Sleep(2000);
+Thread.Sleep(200);
 
 // Happy expression
 Console.WriteLine("Expression: Happy");
 avatar.SetExpression(Expression.Happy);
-Thread.Sleep(3000);
+Thread.Sleep(300);
 
 // Angry expression
 Console.WriteLine("Expression: Angry");
 avatar.SetExpression(Expression.Angry);
-Thread.Sleep(3000);
+Thread.Sleep(300);
 
 // Sleepy expression
 Console.WriteLine("Expression: Sleepy");
 avatar.SetExpression(Expression.Sleepy);
-Thread.Sleep(3000);
+Thread.Sleep(300);
 
 // Sad expression
 Console.WriteLine("Expression: Sad");
@@ -43,13 +43,13 @@ Thread.Sleep(3000);
 Console.WriteLine("Switching to SimpleFace");
 // avatar.SetFace(new SimpleFace());
 avatar.SetExpression(Expression.Neutral);
-Thread.Sleep(3000);
+Thread.Sleep(300);
 
 // Switch to GirlyFace
 Console.WriteLine("Switching to GirlyFace");
 // avatar.SetFace(new GirlyFace());
 avatar.SetExpression(Expression.Happy);
-Thread.Sleep(3000);
+Thread.Sleep(300);
 
 // Demonstrate speech
 Console.WriteLine("Starting speech animation");
