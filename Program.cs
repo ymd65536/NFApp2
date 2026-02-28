d8fc63a9192c5904728bbb7f8a6a41e210c6ed7f
ymd65536 <ymd65536@gmail.com>
2026-02-28 14:23:23 +0900

    LEDバーにRGB点灯処理を追加しusingを整理
    Program.csのusingディレクティブを整理し、不要なものを削除、System.Drawingを追加しました。Fire.LedBarを取得し、whileループ内で最初の3つのLEDを赤・緑・青で点灯させる処理を追加しました。ledBar.Update()で表示を反映しています。

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿using System.Threading;
-using nanoFramework.M5Stack;
-using dotNETM5StackAvatar;
+﻿using dotNETM5StackAvatar;
 using nanoFramework.Hardware.Esp32;
-
+using nanoFramework.M5Stack;
+using System.Drawing;
+using System.Threading;
 using Console = nanoFramework.M5Stack.Console;
 using Math = System.Math;
 
@@ -10,6 +10,7 @@ Fire.InitializeScreen();
 Console.Clear();
 Console.WriteLine("Fast Mouth Animation Example");
 Thread.Sleep(2000);
+var ledBar = Fire.LedBar;
 
 // Initialize the avatar with default face
 var avatar = new Avatar();
@@ -36,4 +37,13 @@ while (true)
     }
 
     Thread.Sleep(50); // 50ms ごとに更新 → 高速な開閉
+
+    var red = Color.FromArgb(255, 255, 0, 0);
+    var green = Color.FromArgb(255, 0, 255, 0);
+    var blue = Color.FromArgb(255, 0, 0, 255);
+
+    ledBar.Image.SetPixel(0, 0, red);
+    ledBar.Image.SetPixel(1, 0, green);
+    ledBar.Image.SetPixel(2, 0, blue);
+    ledBar.Update();
 }
